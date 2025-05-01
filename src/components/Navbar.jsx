97e229230e9ae60e7711fa4e3ef61e8a53a9ab38
GHCLGC <ghc@growth-healthcare.com>
2025-05-01 20:29:48 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
+    <nav className="bg-green-100 shadow p-4 flex items-center justify-between">
+      <Link to="/">
+        <img src="/logo-ligate.png" alt="Logolgc" className="h-10" />
+      </Link>
+      <ul className="flex gap-4 text-green-800 font-semibold">
         <li><Link to="/">Início</Link></li>
         <li><Link to="/login">Login</Link></li>
         <li><Link to="/dashboard">Dashboard</Link></li>
