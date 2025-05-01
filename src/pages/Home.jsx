41382034d378a2f82770dc5a890ea37d4534ea59
GHCLGC <ghc@growth-healthcare.com>
2025-05-01 20:59:27 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,22 +3,22 @@ import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <div className="max-w-3xl mx-auto text-center mt-10 px-4">
+    <div className="max-w-3xl mx-auto text-center mt-10">
       <img
         src="/logo-ligate.png"
         alt="Logo Liga-te ao Coração"
         className="mx-auto h-[30rem] mb-6"
       />
-      <h1 className="text-3xl font-bold text-green-700 mb-4">
-        Liga-te ao Coração
-      </h1>
-      <p className="text-gray-700 mb-6">
+
+      <p className="text-gray-700 mb-2">
         Bem-vindo à plataforma de acompanhamento da pressão arterial nas farmácias.
       </p>
-      <p className="text-sm italic text-gray-500 mb-8">
+
+      <p className="text-sm italic text-gray-500 mb-6">
         Esta é uma aplicação desenvolvida pela KRKA Farmacêutica.
       </p>
-      <div className="flex justify-center">
+
+      <div className="mt-4">
         <Link
           to="/login"
           className="bg-green-600 text-white px-6 py-3 rounded hover:bg-green-700 transition"
