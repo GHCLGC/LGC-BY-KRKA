984a9bec4db2e31f971d3054a3728b89c6ce3b92
GHCLGC <ghc@growth-healthcare.com>
2025-05-04 14:53:17 +0100

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -14,7 +14,7 @@ function Login() {
             <input
               type="text"
               className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm"
-              placeholder="ex: farmacêutico"
+              placeholder="ex: 1234"
             />
           </div>
           <div className="mb-6">
